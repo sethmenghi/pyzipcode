1018bcf9487d8a983d0ff5f8e08a42eb4542a630
Seth Menghi <seth@0ptimus.com>
2016-06-24 12:10:50 -0400

    Update settings.py

--- pyzipcode/settings.py
+++ pyzipcode/settings.py
@@ -1,11 +1,11 @@
 from elect.core.common import query_config
 
 # Assuming we are using same server as elect
-table = 'zipcodes'
-db = 'zipcodes_2015'
-host = query_config('postgres', 'host')
-port = query_config('postgres', 'port')
-user = query_config('postgres', 'user')
-password = query_config('postgres', 'password')
+table = None
+db = None
+host = None
+port = None
+user = None
+password = None
 url = """postgres://{user}:{password}@{host}:{port}/{db}
       """.format(user=user, password=password, host=host, port=port, db=db)
